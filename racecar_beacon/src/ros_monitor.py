bbe960493cf7b4d7e3e98c70281239c9bb92ecaf
emarcoux23 <emarcoux23@hotmail.com>
2023-09-08 23:48:34 -0400

    UDP fonctionnel

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -3,6 +3,8 @@
 import rospy
 import socket
 import threading
+import struct
+import sys
 
 from tf.transformations import euler_from_quaternion
 
@@ -19,7 +21,9 @@ class ROSMonitor:
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.cb_laserscan)
 
         # Current robot state:
-        self.pos = (0,0,0)
+        self.pos_x = 0
+        self.pos_y = 0
+        self.pos_yaw = 0
         self.obstacle = False
         self.id = 0xFAFABEBE
 
@@ -29,31 +33,55 @@ class ROSMonitor:
 
         # Thread for RemoteRequest handling:
         print("Je suis ici")
-        self.pb_thread = threading.Thread(target=self.pb_loop)
-        self.pb_thread.start()
+        self.rr_thread = threading.Thread(target=self.rr_loop)
+        self.rr_thread.start()
+
+        self.pb_init()
 
     def cb_odometry(self, msg:Odometry):
-        (roll, pitch, yaw) = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-        self.pos = (msg.pose.pose.position.x, msg.pose.pose.position.y, yaw)
-        #rospy.loginfo("ros_monitor.py: pos(x, y, yaw): " + str(self.pos))
+        self.pos_x = msg.pose.pose.position.x
+        self.pos_y = msg.pose.pose.position.y
+        (roll, pitch, self.pos_yaw) = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
 
     def cb_laserscan(self, msg:LaserScan): 
-        if min(msg.ranges) <= 1:
-            self.obstacle = True
-            #rospy.logwarn("ros_monitor.py: Obstacle <=1m ?: " + str(self.obstacle))            
+        if min(msg.ranges) <= 1.0:
+            self.obstacle = True          
         else:
-            self.obstacle = False
-            #rospy.loginfo("ros_monitor.py: Obstacle <=1m ?: " + str(self.obstacle))                  
+            self.obstacle = False                  
 
-    def pb_loop(self):
+    def pb_init(self):
         print("pb_loop enter")
-        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
-        self.pb_socket.connect(("127.0.0.1", self.pos_broadcast_port))
-        self.pb_socket.send(bytes(self.pos))                                    #TODO ajouter le reste
-        while True:
-            pass
+        self.server_address = ("192.168.10.255", self.pos_broadcast_port)
+
+        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.pb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        
+        self.timer_pb: rospy.Timer = rospy.Timer(rospy.Duration(1.0), self.pb_send)
+
+    def pb_send(self, some_arg):
+
+        try:
+            self.pos_x = 1.23
+            self.pos_y = 2.34
+            self.pos_yaw = 3.45
+            self.pos_x_bin = struct.pack('!f', self.pos_x)
+            self.pos_y_bin = struct.pack('!f', self.pos_y)
+            self.pos_yaw_bin = struct.pack('!f', self.pos_yaw)
+            self.id_bin = struct.pack('!I', self.id)
+
+            self.pos_send = self.pos_x_bin + self.pos_y_bin + self.pos_yaw_bin + self.id_bin
+            self.pb_socket.sendto(self.pos_send, self.server_address)
+
+            rospy.loginfo("Sending to " + self.server_address[0])
+
+        except Exception as e:
+            rospy.logerr(e)
+
+    def rr_loop(self, some_arg):
+        self.rr_socket.send("RPOS") #TODO modifier
 
 if __name__=="__main__":
     rospy.init_node("ros_monitor")
     node = ROSMonitor()
     rospy.spin()
+    node.pb_socket.close()
--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
 
 import socket
+import struct
 
-HOST = '127.0.0.1'
+HOST = '127.0.0.1' #'192.168.10.1'
 # This process should listen to a different port than the RemoteRequest client.
 PORT = 65431
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+#server_socket.listen(1)
 
 try:
     while True:
-        print("Hoooooooooo")
-        conn, addr = server_socket.accept()
-        with conn:
-            while True:
-                print("Hiiiiiiiiiiii")
-                data = conn.recv(16)
-                if not data : break
-                print("Haaaaaaaaaaaaaaaaaa")
-                conn.sendall(data)
+        data, addr = server_socket.recvfrom(16)
+        data_x = struct.unpack('!f', data[0:4])[0]
+        data_y = struct.unpack('!f', data[4:8])[0]
+        data_yaw = struct.unpack('!f', data[8:12])[0]
+        data_id = struct.unpack('!I', data[12:16])[0]
+
+        print("received message: x:" + str(data_x) + " - y:" + str(data_y) + " - yaw:" + str(data_yaw) + " - id:" + str(data_id))
+
 
 except KeyboardInterrupt:
+    server_socket.close()
     exit()
